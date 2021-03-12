b105a4b6c6f22edb5c36233acbe956fa20761f1c
shushhy <49201453+shushhy@users.noreply.github.com>
2021-03-12 15:46:59 +0000

    Models for Customer, Orders, Order_Details and Product

---
+++ OSSApi/OSSApi/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OSSApi.Models {
+    public class Customer {
+        public Int32 customer_id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public char gender { get; set; }
+        public string country { get; set; }
+        public string phone_number { get; set; }
+    }
+}
---
+++ OSSApi/OSSApi/Models/Order_Details.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OSSApi.Models {
+    public class Order_Details {
+        public Int32 order_details_id { get; set; }
+        public Int32 product_id { get; set; }
+        public Int64 order_id { get; set; }
+        public Int16 order_details_quantity { get; set; }
+        public decimal order_details_price { get; set; }
+    }
+}
