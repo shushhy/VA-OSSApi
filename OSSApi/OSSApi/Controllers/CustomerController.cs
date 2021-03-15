1626ad19e951a03204dd672928385d95562ff552
shushhy <49201453+shushhy@users.noreply.github.com>
2021-03-15 14:41:46 +0000

    clean up

--- OSSApi/OSSApi/Controllers/CustomerController.cs
+++ OSSApi/OSSApi/Controllers/CustomerController.cs
@@ -27,34 +27,5 @@ namespace OSSApi.Controllers {
             };
             return Ok("failed");
         }
-
-
-
-
-
-
-
-
-
-        // GET api/<CustomerController>/5
-        [HttpGet("{id}")]
-        public string Get(int id) {
-            return "value";
-        }
-
-        // POST api/<CustomerController>
-        [HttpPost]
-        public void Post([FromBody] string value) {
-        }
-
-        // PUT api/<CustomerController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value) {
-        }
-
-        // DELETE api/<CustomerController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id) {
-        }
     }
 }
