9c2142f8e88d36b21cd34ca74f3dc43f9e617cf3
Filipe Fernandes <filipe.fernandes@vanameyde.com>
2021-03-22 11:19:51 +0000

    Product CRUD - Error

--- OSSApi/OSSApi/Controllers/ProductController.cs
+++ OSSApi/OSSApi/Controllers/ProductController.cs
@@ -5,11 +5,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using OSS.Data.Repository;
+using OSS.Core.Models;
 
-namespace OSSApi.Controllers {
+namespace OSSApi.Controllers
+{
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductController : ControllerBase {
-        
+    public class ProductController : ControllerBase
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        // GetAll Product
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var data = await unitOfWork.Products.GetAll();
+            return Ok(data);
+        }
+
+        // GetById Product
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var data = await unitOfWork.Products.GetById(id);
+            if (data == null)
+            {
+                return BadRequest();
+            }
+            return Ok(data);
+        }
+
+        // Post Product
+        [HttpPost]
+        public async Task<IActionResult> Insert(Product entity)
+        {
+            var data = await unitOfWork.Products.Insert(entity);
+            return Ok(data);
+        }
+
+        // Put Product
+        [HttpPut]
+        public async Task<IActionResult> Update(Product entity)
+        {
+            var data = await unitOfWork.Products.Update(entity);
+            return Ok(data);
+        }
+
+        // Delete Product
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var data = await unitOfWork.Products.Delete(id);
+            return Ok(data);
+        }
     }
 }
